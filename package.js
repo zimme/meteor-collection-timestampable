f407c8da1a7fc9483e7e89550dfd8ccd37619d1c
Simon Fridlund <simon.r.k.fridlund@gmail.com>
2014-11-11 02:30:40 +0100

    Add package.js

---
+++ package.js
@@ -0,0 +1,28 @@
+Package.describe({
+  git: 'https://github.com/zimme/meteor-collection-timestampable',
+  name: 'zimme:collection-timestampable',
+  summary: 'Adds timestamps to collections',
+  version: '1.0.0'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.0');
+
+  api.use([
+    'coffeescript',
+    'underscore'
+  ]);
+
+  api.use([
+    'matb33:collection-hooks@0.7.6',
+    'zimme:collection-behaviours@1.0.0'
+  ]);
+
+  api.use([
+    'aldeed:autoform@4.0.0',
+    'aldeed:collection2@2.0.0',
+    'aldeed:simple-schema@1.0.3'
+  ], ['client', 'server'], {weak: true});
+
+  api.addFiles('timestampable.coffee');
+});
